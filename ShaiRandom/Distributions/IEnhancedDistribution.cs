fa159ff9fda9e30b168e808fda3733a1f355f803
Chris3606 <cridley106@gmail.com>
2021-12-30 19:20:01 -0600

    Modified distribution interfaces to implement/extend IDistribution from Troschuetz.  Unsupported and replaced functionality is implemented explicitly to avoid cluttering the API.

--- ShaiRandom/Distributions/IEnhancedDistribution.cs
+++ ShaiRandom/Distributions/IEnhancedDistribution.cs
@@ -1,19 +1,19 @@
 ﻿/*
  * MIT License
- * 
+ *
  * Copyright (c) 2006-2007 Stefan Troschuetz <stefan@troschuetz.de>
  * Copyright (c) 2012-2021 Alessio Parma <alessio.parma@gmail.com>
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,86 +23,45 @@
  * SOFTWARE.
  */
 using System;
+using Troschuetz.Random;
 
 namespace ShaiRandom.Distributions
 {
 
     /// <summary>
-    ///   Declares common functionality for all continuous random number distributions.
-    /// </summary>
-    public interface IContinuousDistribution : IDistribution
-    {
-    }
-
-    /// <summary>
     ///   Declares common functionality for all discrete random number distributions.
     /// </summary>
-    public interface IDiscreteDistribution : IDistribution
+    public interface IEnhancedDiscreteDistribution : IEnhancedDistribution, IDiscreteDistribution
     {
         /// <summary>
         ///   Returns a distributed random number.
         /// </summary>
         /// <returns>A distributed 32-bit signed integer.</returns>
         int NextInt();
+
+        #region IDiscreteDistribution Explicit Implementation
+        /// <inheritdoc />
+        int IDiscreteDistribution.Next() => NextInt();
+        #endregion
+
+    }
+
+    /// <summary>
+    ///   Declares common functionality for all continuous random number distributions.
+    /// </summary>
+    public interface IEnhancedContinuousDistribution : IEnhancedDistribution
+    {
     }
 
     /// <summary>
     ///   Declares common functionality for all random number distributions.
     /// </summary>
-    public interface IDistribution
+    public interface IEnhancedDistribution : IDistribution
     {
         /// <summary>
         ///   Gets the <see cref="IEnhancedRandom"/> object that is used as underlying random number generator.
         /// </summary>
-        IEnhancedRandom Generator { get; }
-
-        /// <summary>
-        ///   Gets the maximum possible value of distributed random numbers.
-        /// </summary>
-        double Maximum { get; }
-
-        /// <summary>
-        ///   Gets the mean of distributed random numbers.
-        /// </summary>
-        /// <exception cref="NotSupportedException">
-        ///   Thrown if mean is not defined for given distribution with some parameters.
-        /// </exception>
-        double Mean { get; }
-
-        /// <summary>
-        ///   Gets the median of distributed random numbers.
-        /// </summary>
-        /// <exception cref="NotSupportedException">
-        ///   Thrown if median is not defined for given distribution with some parameters.
-        /// </exception>
-        double Median { get; }
-
-        /// <summary>
-        ///   Gets the minimum possible value of distributed random numbers.
-        /// </summary>
-        double Minimum { get; }
-
-        /// <summary>
-        ///   Gets the mode of distributed random numbers.
-        /// </summary>
-        /// <exception cref="NotSupportedException">
-        ///   Thrown if mode is not defined for given distribution with some parameters.
-        /// </exception>
-        double[] Mode { get; }
-
-        /// <summary>
-        ///   Gets the variance of distributed random numbers.
-        /// </summary>
-        /// <exception cref="NotSupportedException">
-        ///   Thrown if variance is not defined for given distribution with some parameters.
-        /// </exception>
-        double Variance { get; }
-
-        /// <summary>
-        ///   Returns a distributed floating point random number.
-        /// </summary>
-        /// <returns>A distributed double-precision floating point number.</returns>
-        double NextDouble();
+        new IEnhancedRandom Generator { get; }
 
         /// <summary>
         /// How many steps the IEnhancedRandom generator advances each time it produces a distributed result, if a constant value.
@@ -141,5 +100,16 @@ namespace ShaiRandom.Distributions
         /// Thrown if the parameter by the given index does not exist.
         /// </exception>
         void SetParameterValue(int index, double value);
+
+        #region IDistribution Explicit Method Implementations
+        /// <inheritdoc />
+        bool IDistribution.CanReset => false;
+
+        /// <inheritdoc />
+        IGenerator IDistribution.Generator => throw new NotImplementedException(); // Need to implement this after IEnhancedRandom implements IGenerator
+
+        /// <inheritdoc />
+        bool IDistribution.Reset() => false;
+        #endregion
     }
 }
